4f7f870ee7dfdb53581053b71f52603ce2940399
codeparameter <codeparameter.com@gmail.com>
2023-02-10 09:04:07 +0330

    helper urls has current url now

--- helpers/urls.php
+++ helpers/urls.php
@@ -3,3 +3,7 @@
 function site_url($route){
     return $_ENV['HOST'] . $route;
 }
+
+function current_url(){
+    return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+}
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 include "bootstrap/init.php";
 print_r($_ENV);
-echo site_url('test');
+echo site_url('test') . '<hr>';
+echo $_SERVER['REQUEST_URI'];
