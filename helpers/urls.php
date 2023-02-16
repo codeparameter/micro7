4dad34955bdb658fe2bf78089ab683268e609086
codeparameter <codeparameter.com@gmail.com>
2023-02-16 10:59:26 +0330

    htaccess added

--- helpers/urls.php
+++ helpers/urls.php
@@ -7,3 +7,7 @@ function site_url($route){
 function current_url(){
     return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 }
+
+function current_route(){
+    return strtok($_SERVER['REQUEST_URI'], '?');
+}
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include "bootstrap/init.php";
 print_r($_ENV);
-echo site_url('test') . '<hr>';
-echo $_SERVER['REQUEST_URI'];
+echo '<hr>' . site_url('test') . '<hr>';
+echo $_SERVER['REQUEST_URI'] . '<hr>';
+echo current_route() . '<hr>';
