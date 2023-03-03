932fed65c6efff0f789df2f8a10e62433be1fc1e
codeparameter <codeparameter.com@gmail.com>
2023-03-03 11:25:03 +0330

    script and stylesheet functions added to views.php helper

--- helpers/views.php
+++ helpers/views.php
@@ -4,3 +4,11 @@ function view($path, $data = []){
     extract($data);
     include_once BASEPATH . "views/$path.php";
 }
+
+function stylesheet($path){
+    echo '<link rel="stylesheet" href="assets/css/' . $path . '"/>';
+}
+
+function script($path){
+    echo '<script src="assets/js/' . $path . '"></script>';
+}
