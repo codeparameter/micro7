a69623655ddaca87fd3ef1a8b140ed55eebc3469
codeparameter <codeparameter.com@gmail.com>
2023-03-08 13:16:26 +0330

    array query issue fixed

--- helpers/tools.php
+++ helpers/tools.php
@@ -1,28 +1,46 @@
 <?php
 
-function _global($var_name){
+function _global($var_name)
+{
     global ${$var_name};
     return ${$var_name};
 }
 
-function parseToInt($num){
+function parseToInt($num)
+{
     return (int)$num;
 }
 
-function xss_clean($str){
+function xss_clean_string($str)
+{
     return filter_var(htmlspecialchars($str), FILTER_SANITIZE_STRING);
 }
 
-function json($data){
+function xss_clean_array($stringArray){
+    if(count($stringArray) != 1)
+        return null;
+    return array_map('xss_clean_string', explode(',', $stringArray[0]));
+}
+
+function xss_clean($value)
+{
+    return is_array($value) ?
+        xss_clean_array($value) : xss_clean_string($value);
+}
+
+function json($data)
+{
     header('Content-Type: application/json; charset=utf-8');
     echo json_encode($data);
     die();
 }
 
-function jsonHeader($status, $message = '', $data = []){
+function jsonHeader($status, $message = '', $data = [])
+{
     json(
         array_merge(
-            ['status' => $status, 'message' => $message], $data
+            ['status' => $status, 'message' => $message],
+            $data
         )
     );
 }
